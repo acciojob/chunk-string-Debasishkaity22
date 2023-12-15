2357d4b45fec77f1957d3b28ef2bc83e1fa3a37e
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-15 11:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function stringChop(str, size) {
   // your code here
+	let l=[],m="";
+  let n;
+  for(let i=0;i<str.length;i=(i+size)){
+    let n1=parseInt(i);
+    let n2=parseInt(size);
+     n=(n1+n2);
+    m=str.substring(i,n);
+    l.push(m);
+  }
+  if(n<str.length){
+    m=str.substring(n,str.length);
+    l.push(m);
+  }
+  return l;
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");

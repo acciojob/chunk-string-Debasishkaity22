d5fb803d6ff93d9a57111d336e5a33ffc611c333
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-15 14:31:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function stringChop(str, size) {
   // your code here
 	let l=[],m="";
   let n;
-	if(str==="null"||str===undefined) return [];
+	if(str===""||str===undefined) return [];
    else{	
   for(let i=0;i<str.length;i=(i+size)){
     let n1=parseInt(i);
